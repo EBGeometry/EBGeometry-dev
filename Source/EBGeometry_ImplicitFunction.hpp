59b2cd90c5567a80bce4cf5feca174eef7190818
Robert Marskar <robert.marskar@gmail.com>
2024-09-19 19:42:18 +0200

    moved functions around

--- Source/EBGeometry_ImplicitFunction.hpp
+++ Source/EBGeometry_ImplicitFunction.hpp
@@ -23,6 +23,50 @@
 
 namespace EBGeometry {
 
+#ifdef EBGEOMETRY_ENABLE_GPU  
+  /*!
+    @brief Shortcut for GPU pointer. Note that this is mostly used so that users
+    know from the typename which pointers are intended to be pointers to data allocated
+    on the device.
+  */
+  template <typename T>
+  using GPUPointer = T**;
+#endif
+
+
+#ifdef EBGEOMETRY_ENABLE_GPU
+  /*!
+    @brief Allocate pointer for implicit function on the GPU
+    @param[inout] implicitFunction Implicit function pointer to be allocated. 
+  */
+  template <typename T>
+  EBGEOMETRY_GPU_HOST inline GPUPointer<T>
+  allocateImplicitFunctionOnDevice() noexcept
+  {
+    GPUPointer<T> func;
+
+    cudaMalloc((void**)&func, sizeof(GPUPointer<T>));
+
+    return func;
+  }
+#endif
+
+#ifdef EBGEOMETRY_ENABLE_GPU
+  /*!
+    @brief Function for building an arbitrary implicit function. Used when constructing
+    implicit functions on the GPU. The user inputs the implicit function type (T) and the
+    constructor arguments required for constructing the function.
+    @param[in] a_implicitFunction Implicit function to be created on device
+    @param[in] args Constructor arguments.
+  */
+  template <typename T, typename... Args>
+  EBGEOMETRY_GPU_GLOBAL void
+  createImplicitFunctionOnDevice(GPUPointer<T> a_implicitFunction, Args... args)
+  {
+    (*a_implicitFunction) = new T(*args...);
+  };
+#endif    
+
   /*!
     @brief Implicit function base class. 
     @details An implicit function must have the following properties:
