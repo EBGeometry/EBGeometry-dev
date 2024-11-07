0c32e063c41bf5ecfe635b3807bbc4b67183e0e1
Robert Marskar <robert.marskar@gmail.com>
2024-11-07 10:30:29 +0100

    Start adding virtual function tests -- must ensure that we have a good factory pattern for GPU instantiation

---
+++ Exec/Tests/VirtualFunctionTests.cpp
@@ -0,0 +1,11 @@
+#include "EBGeometry.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
+
+using namespace EBGeometry;
+
+TEST_CASE("VirtualFunctions_CPU")
+{
+  CHECK(true);
+}
