762b3160b7c5656bf10dfbb54e9a73b26248edfa
Robert Marskar <robert.marskar@sintef.no>
2025-08-20 08:59:37 +0200

    update

--- Exec/Examples/EBGeometry_Shapes/Sphere.cpp
+++ Exec/Examples/EBGeometry_Shapes/Sphere.cpp
@@ -1,8 +1,18 @@
 #include "EBGeometry.hpp"
 
+using namespace EBGeometry;
+
 int main() {
 
-  std::cout << "hello world" << std::endl;
+  auto sphereHost = createImpFunc<SphereSDF, MemoryLocation::Host>(Vec3::zero(), Real(0.5));
+
+  std::cout << "Evaluation host: value = " << sphereHost->value(Vec3::one()) << "\n";
+
+  freeImpFunc(sphereHost);  
+
+#ifdef EBGEOMETRY_ENABLE_GPU
+  auto sphereDevice = createImpFunc<SphereSDF, MemoryLocation::Pinned>(Vec3::zero(), Real(1.0));
+#endif
   
   return 0;
 }
