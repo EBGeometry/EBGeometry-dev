e7171536e5bc33539644413c8d8c9182a9dc09f3
Robert Marskar <robert.marskar@gmail.com>
2025-09-27 19:45:27 +0200

    docs

--- Source/EBGeometry_Types.hpp
+++ Source/EBGeometry_Types.hpp
@@ -4,7 +4,20 @@
 
 /**
  * @file   EBGeometry_Types.hpp
- * @brief  Compile-time precision specification
+ * @author Robert Marskar
+ * @brief  Compile-time precision specification for EBGeometry.
+ *
+ * This header defines the floating-point type (`Real`) and associated
+ * compile-time constants used within EBGeometry. The precision (single
+ * or double) is determined at compile time via the `EBGEOMETRY_USE_DOUBLE`
+ * preprocessor flag.
+ *
+ * - If `EBGEOMETRY_USE_DOUBLE` is defined, `Real` is `double`.
+ * - Otherwise, `Real` is `float`.
+ *
+ * The file also provides maximum, minimum, lowest, and epsilon constants
+ * for the selected type.
+ *
  * @author Robert Marskar
  */
 
@@ -14,27 +27,69 @@
 #include <cfloat>
 
 /**
- * \cond
+ * @namespace EBGeometry
+ * @brief Namespace containing types and constants for EBGeometry.
  */
 #ifdef EBGEOMETRY_USE_DOUBLE
 namespace EBGeometry {
-  using Real                 = double;
+  /**
+   * @typedef Real
+   * @brief Floating-point type used throughout EBGeometry.
+   *
+   * Defined as `double` when `EBGEOMETRY_USE_DOUBLE` is enabled.
+   */
+  using Real = double;
+
+  /**
+   * @brief Maximum representable value of `Real`.
+   */
   constexpr Real MaximumReal = DBL_MAX;
+
+  /**
+   * @brief Minimum positive normalized value of `Real`.
+   */
   constexpr Real MinimumReal = DBL_MIN;
-  constexpr Real LowestReal  = -DBL_MAX;
-  constexpr Real Epsilon     = DBL_EPSILON;
+
+  /**
+   * @brief Lowest finite value of `Real` (most negative).
+   */
+  constexpr Real LowestReal = -DBL_MAX;
+
+  /**
+   * @brief Difference between 1.0 and the next representable value of `Real`.
+   */
+  constexpr Real Epsilon = DBL_EPSILON;
 } // namespace EBGeometry
 #else
 namespace EBGeometry {
-  using Real                 = float;
+  /**
+   * @typedef Real
+   * @brief Floating-point type used throughout EBGeometry.
+   *
+   * Defined as `float` when `EBGEOMETRY_USE_DOUBLE` is not enabled.
+   */
+  using Real = float;
+
+  /**
+   * @brief Maximum representable value of `Real`.
+   */
   constexpr Real MaximumReal = FLT_MAX;
+
+  /**
+   * @brief Minimum positive normalized value of `Real`.
+   */
   constexpr Real MinimumReal = FLT_MIN;
-  constexpr Real LowestReal  = -FLT_MAX;
-  constexpr Real Epsilon     = FLT_EPSILON;
+
+  /**
+   * @brief Lowest finite value of `Real` (most negative).
+   */
+  constexpr Real LowestReal = -FLT_MAX;
+
+  /**
+   * @brief Difference between 1.0 and the next representable value of `Real`.
+   */
+  constexpr Real Epsilon = FLT_EPSILON;
 } // namespace EBGeometry
 #endif
-/**
- * \endcond
- */
 
 #endif
